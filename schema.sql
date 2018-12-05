6067cfd0c3c6d7a6356b459da03448909e90a3b3
Shershnev Sergey <shv.sergey70@gmail.com>
2018-12-05 14:48:44 +0300

    module4-task2 Добавил данные в БД и подготовил запросы v1

--- schema.sql
+++ schema.sql
@@ -10,22 +10,28 @@ author_id INT
 
 CREATE TABLE tasks (
 id INT AUTO_INCREMENT PRIMARY KEY,
-create_datetime DATETIME,
+name TEXT,
+status TINYINT DEFAULT 0,
+create_datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
 finish_datetime DATETIME,
-STATUS TINYINT DEFAULT 0,
-NAME TEXT,
-FILE_URL VARCHAR(255),
 deadline_datetime DATETIME,
+file_url VARCHAR(255),
 author_id INT,
 project_id INT
 );
 
 CREATE TABLE users (
 id INT AUTO_INCREMENT PRIMARY KEY,
-register_datetime DATETIME,
+register_datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
 email VARCHAR(100),
 name CHAR(150),
 password CHAR(150),
 contacts TEXT,
 UNIQUE(`email`)
 );
+
+CREATE INDEX deadline_datetime ON tasks(deadline_datetime);
+CREATE INDEX author_id ON tasks(author_id);
+CREATE INDEX project_id ON tasks(project_id);
+
+CREATE INDEX author_id ON projects(author_id);
